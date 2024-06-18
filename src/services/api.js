7566cfa97b5601183d9ffdb0d89c1d9da0450203
Anthony <anthony.silva@avmb.com.br>
2024-06-18 08:34:07 -0300

    feat - Configurando deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-
     baseURL: 'http://localhost:3000'
+    // baseURL: 'https://project3-2024a-anthony-luizfelipe-backend-mzvs-g87tdrkg2.vercel.app'
 })
 
 export default api
