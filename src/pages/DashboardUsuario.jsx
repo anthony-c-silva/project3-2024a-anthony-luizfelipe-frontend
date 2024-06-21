2c9b6d66a0ea7e58701563205f8e4395b0c7535b
Luiz Felipe Cavalheiro <lfsantos@inf.ufsm.br>
2024-06-21 14:28:36 -0300

    ajustes no front para 27/06

--- src/pages/DashboardUsuario.jsx
+++ src/pages/DashboardUsuario.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './DashboardAbrigo.css';
 import api from '../services/api.js';
 import { useNavigate } from 'react-router-dom';
@@ -11,11 +11,15 @@ function DashboardUsuario() {
     const [showModal, setShowModal] = useState(false);
     const [showSearchModal, setShowSearchModal] = useState(false);
     const [novoAbrigo, setNovoAbrigo] = useState({
+        id: null,
         nome: '',
         localizacao: ''
     });
     const [searchCriteria, setSearchCriteria] = useState('id');
     const [searchValue, setSearchValue] = useState('');
+    const [duplicateAbrigo, setDuplicateAbrigo] = useState(null); // Estado para armazenar o abrigo duplicado encontrado
+    const [errors, setErrors] = useState({}); // Estado para armazenar mensagens de erro de validação
+    const [deleteConfirmationName, setDeleteConfirmationName] = useState(''); // Estado para armazenar o nome de confirmação de exclusão
 
     const navigate = useNavigate();
 
@@ -36,11 +40,25 @@ function DashboardUsuario() {
     // Função assíncrona para criar um abrigo
     async function createAbrigo() {
         try {
+            // Verifica se há campos vazios
+            if (!validateForm()) {
+                return;
+            }
+
+            // Verifica se já existe um abrigo com o mesmo nome
+            const abrigoExistente = abrigos.find(abrigo => abrigo.nome.toLowerCase() === novoAbrigo.nome.toLocaleLowerCase());
+            if (abrigoExistente) {
+                setDuplicateAbrigo(abrigoExistente); // Armazena o abrigo duplicado encontrado
+                setShowModal(false); // Fecha o modal de adição/edição
+                setShowDuplicateModal(true); // Abre o modal de confirmação de edição
+                return; // Interrompe o fluxo aqui para não continuar com a criação
+            }
+
             await api.post('/api/abrigos', {
                 nome: novoAbrigo.nome,
                 localizacao: novoAbrigo.localizacao,
             });
-            getAbrigos();
+            await getAbrigos(); // Atualiza a lista de abrigos após a criação
             closeModal();
         } catch (error) {
             console.error('Erro ao criar abrigo:', error);
@@ -54,7 +72,7 @@ function DashboardUsuario() {
                 nome: novoAbrigo.nome,
                 localizacao: novoAbrigo.localizacao,
             });
-            getAbrigos();
+            await getAbrigos(); // Atualiza a lista de abrigos após a atualização
             closeModal();
         } catch (error) {
             console.error(`Erro ao atualizar abrigo com ID ${novoAbrigo.id}:`, error);
@@ -65,7 +83,7 @@ function DashboardUsuario() {
     async function deleteAbrigo(id) {
         try {
             await api.delete(`/api/abrigos/${id}`);
-            getAbrigos();
+            await getAbrigos(); // Atualiza a lista de abrigos após a exclusão
         } catch (error) {
             console.error(`Erro ao deletar abrigo com ID ${id}:`, error);
         }
@@ -75,12 +93,15 @@ function DashboardUsuario() {
     function openModal(abrigo = { id: null, nome: '', localizacao: '' }) {
         setNovoAbrigo(abrigo);
         setShowModal(true);
+        setDuplicateAbrigo(null); // Limpa o estado do abrigo duplicado ao abrir o modal
+        setErrors({}); // Limpa os erros ao abrir o modal
     }
 
     // Função para fechar o modal
     function closeModal() {
         setShowModal(false);
         setNovoAbrigo({ id: null, nome: '', localizacao: '' });
+        setErrors({}); // Limpa os erros ao fechar o modal
     }
 
     // Função para lidar com a mudança nos campos do formulário
@@ -122,6 +143,63 @@ function DashboardUsuario() {
         }
     }
 
+    // Função para validar o formulário antes da criação ou atualização
+    function validateForm() {
+        let formErrors = {};
+        if (!novoAbrigo.nome) formErrors.nome = "Nome é obrigatório";
+        if (!novoAbrigo.localizacao) formErrors.localizacao = "Localização é obrigatória";
+        setErrors(formErrors);
+        return Object.keys(formErrors).length === 0;
+    }
+
+    // Função para abrir o modal de confirmação de exclusão
+    function openDeleteModal(abrigo) {
+        setDeleteConfirmationName('');
+        setNovoAbrigo(abrigo);
+        setShowDeleteModal(true);
+    }
+
+    // Função para fechar o modal de confirmação de exclusão
+    function closeDeleteModal() {
+        setShowDeleteModal(false);
+        setDeleteConfirmationName('');
+    }
+
+    // Função para lidar com a exclusão do abrigo após confirmação
+    async function handleDeleteConfirmation() {
+        if (deleteConfirmationName.toLowerCase() === novoAbrigo.nome.toLowerCase()) {
+            await deleteAbrigo(novoAbrigo.id);
+            setShowDeleteModal(false);
+        } else {
+            setShowDeleteModal(false);
+            setShowNameMismatchModal(true);
+        }
+    }
+
+    // Estado para controlar o modal de confirmação de edição do abrigo duplicado
+    const [showDuplicateModal, setShowDuplicateModal] = useState(false);
+    // Estado para controlar o modal de confirmação de nome incorreto
+    const [showNameMismatchModal, setShowNameMismatchModal] = useState(false);
+    // Estado para controlar o modal de exclusão
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+    // Função para fechar o modal de confirmação de edição
+    function closeDuplicateModal() {
+        setShowDuplicateModal(false);
+    }
+
+    // Função para fechar o modal de confirmação de nome incorreto
+    function closeNameMismatchModal() {
+        setShowNameMismatchModal(false);
+    }
+
+    // Função para confirmar a edição do abrigo duplicado
+    function confirmEdit() {
+        setNovoAbrigo({ ...duplicateAbrigo });
+        setShowModal(true); // Abrir modal de adição/edição com os dados do abrigo duplicado
+        setShowDuplicateModal(false); // Fechar modal de confirmação de edição
+    }
+
     useEffect(() => {
         getAbrigos();
     }, []);
@@ -152,13 +230,13 @@ function DashboardUsuario() {
                                 <td>{abrigo.localizacao}</td>
                                 <td>
                                     <button className='icon-button' onClick={() => openModal(abrigo)}>
-                                        <img src={Edit}/>
+                                        <img src={Edit} alt="Editar" />
                                     </button>
-                                    <button className='icon-button' onClick={() => deleteAbrigo(abrigo.id)}>
-                                        <img src={Trash}/>
+                                    <button className='icon-button' onClick={() => openDeleteModal(abrigo)}>
+                                        <img src={Trash} alt="Excluir" />
                                     </button>
                                     <button className='icon-button' onClick={() => handleVisualizar(abrigo.id)}>
-                                        <img src={View}/>
+                                        <img src={View} alt="Visualizar" />
                                     </button>
                                 </td>
                             </tr>
@@ -180,6 +258,7 @@ function DashboardUsuario() {
                                         value={novoAbrigo.nome}
                                         onChange={handleChange}
                                     />
+                                    {errors.nome && <span className="error">{errors.nome}</span>}
                                 </label>
                                 <label>
                                     Localização:
@@ -189,6 +268,7 @@ function DashboardUsuario() {
                                         value={novoAbrigo.localizacao}
                                         onChange={handleChange}
                                     />
+                                    {errors.localizacao && <span className="error">{errors.localizacao}</span>}
                                 </label>
                                 <button type="submit">Salvar</button>
                             </form>
@@ -221,6 +301,52 @@ function DashboardUsuario() {
                                 </label>
                                 <button type="submit">Pesquisar</button>
                             </form>
+                        </div>
+                    </div>
+                )}
+
+                {/* Modal para confirmação de exclusão */}
+                {showDeleteModal && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeDeleteModal}>&times;</span>
+                            <h3>Excluir Abrigo</h3>
+                            <h4>Digite o nome do abrigo para confirmar a exclusão:</h4>
+                            <input
+                                type="text"
+                                value={deleteConfirmationName}
+                                onChange={(e) => setDeleteConfirmationName(e.target.value)}
+                                placeholder="Digite o nome do abrigo"
+                            />
+                            <div>
+                                <button onClick={handleDeleteConfirmation}>Excluir</button>
+                                <button onClick={closeDeleteModal}>Cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {/* Modal para informar que o nome digitado está incorreto */}
+                {showNameMismatchModal && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeNameMismatchModal}>&times;</span>
+                            <h3>Nome Incorreto</h3>
+                            <h4>O nome digitado não corresponde ao nome do abrigo "{novoAbrigo.nome}".</h4>
+                            <button onClick={closeNameMismatchModal}>OK</button>
+                        </div>
+                    </div>
+                )}
+
+                {/* Modal para confirmação de edição do abrigo duplicado */}
+                {showDuplicateModal && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeDuplicateModal}>&times;</span>
+                            <h3>Abrigo Duplicado Encontrado</h3>
+                            <h4>Já existe um abrigo com o nome "{duplicateAbrigo.nome}". Deseja editar este abrigo?</h4>
+                            <button onClick={confirmEdit}>Editar Abrigo</button>
+                            <button onClick={closeDuplicateModal}>Cancelar</button>
                         </div>
                     </div>
                 )}
