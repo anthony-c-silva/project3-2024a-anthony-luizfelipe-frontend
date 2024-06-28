e6811ecf6e8de4fd89311201ba3e694f5cb61dbe
anthony-c-silva <anthonycarlosp@gmail.com>
2024-06-28 20:22:10 -0300

    refactor - refatorando obj itens

--- src/pages/DashboardAbrigo.jsx
+++ src/pages/DashboardAbrigo.jsx
@@ -60,7 +60,7 @@ function DashboardAbrigo() {
                 setShowEditModal(true); // Abre o modal de confirmação de edição
                 return; // Interrompe o fluxo aqui para não continuar com a criação
             }
-            console.log(novoItem);
+            console.log(novoItem.abrigoId + Number(novoItem.quantidade));
             await api.post('/itens',{ 
                     nome: novoItem.nome,
                     quantidade: Number(novoItem.quantidade),
