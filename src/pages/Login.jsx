f787b9a39722023faf0248ed656164fd6ac63533
Anthony <anthony.silva@avmb.com.br>
2024-07-04 09:55:04 -0300

    feat - criando login usuario-adm e token e validaçao

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,62 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import {jwtDecode} from "jwt-decode";
 
 function Login() {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    console.log('Username:', username);
-    console.log('Password:', password);
-    navigate('/dashboard-usuario');
+  const handleLogin = async () => {
+    if (!email || !password) {
+      setError('Email e senha são obrigatórios');
+      return;
+    }
+
+    const data = {
+      email: email,
+      senha: password,
+    };
+
+    try {
+      const response = await axios.post('https://project3-2024a-anthony-luizfelipe-backend.onrender.com/login', data);
+      const { token } = response.data;
+      localStorage.setItem('token', token);
+
+      const decodedToken = jwtDecode(token);
+      const { id } = decodedToken;
+
+      navigate(`/dashboard-abrigo/${id}`);
+    } catch (error) {
+      setError('Erro ao fazer login. Verifique suas credenciais.');
+    }
   };
 
   return (
-    <>
     <div className="login-container">
-        <h2>Login</h2>
-        <div>
-            <input type="text" placeholder="Username"/>
-        </div>
-        <div>
-            <input type="password" placeholder="Password"/>
-        </div>
-        <button className="login-button" onClick={handleLogin}>Login</button>
+      <h2>Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <div>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="login-button" onClick={handleLogin}>Login</button>
     </div>
-    </>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,13 @@ const api = axios.create({
     baseURL: 'https://project3-2024a-anthony-luizfelipe-backend.onrender.com'
 })
 
+// Configuração global para incluir o token de autorização em todas as requisições
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+  
 export default api
