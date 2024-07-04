76e2c6a7c7b115e9657d30eeed49bc2dc762d38b
Anthony <anthony.silva@avmb.com.br>
2024-07-04 09:54:11 -0300

    feat - criando cadastro admin

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,56 +1,83 @@
 import React, { useState } from 'react';
 import './Cadastro.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Cadastro() {
-  const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [tipo, setTipo] = useState('');
-  const  navigate = useNavigate();
-  
+  const [abrigoNome, setAbrigoNome] = useState('');
+  const [abrigoEndereco, setAbrigoEndereco] = useState('');
+  const navigate = useNavigate();
 
-  const handleCadastro = () => {
-    console.log('Nome:', nome);
-    console.log('Email:', email);
-    console.log('Username:', username);
-    console.log('Password:', password);
-    console.log('Tipo:', tipo);
-    navigate('/dashboard-usuario');
-  };
+  const handleCadastro = async () => {
+    const data = {
+      nomeUsuario: username,
+      senha: password,
+      email: email,
+      abrigo: {
+        nome: abrigoNome,
+        endereco: abrigoEndereco,
+      },
+    };
 
-  const handleChangeTipo = (event) => {
-    setTipo(event.target.value);
+    try {
+      await axios.post('https://project3-2024a-anthony-luizfelipe-backend.onrender.com/primeiro-admin', data);
+      navigate('/login');
+    } catch (error) {
+      console.error('Erro ao cadastrar administrador:', error);
+    }
   };
 
   return (
-    <>
     <div className="cadastro-container">
-        <h2>Cadastro</h2>
-        <div>
-            <input type="text" placeholder="Nome"/>
-        </div>
-        <div>
-            <input type="email" placeholder="Email"/>
-        </div>
-        <div>
-            <input type="text" placeholder="Username"/>
-        </div>
-        <div>
-            <input type="password" placeholder="Password"/>
-        </div>
-        <div>
-            <select value={tipo} onChange={handleChangeTipo}>
-                <option value=""> Selecione...</option>
-                <option value="voluntario"> Voluntário</option>
-                <option value="administrador"> Administrador</option>
-            </select>
-        </div>
-
-        <button className="signup-button" onClick={handleCadastro}>Cadastro</button>
+      <h2>Cadastro de Administrador</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Nome do Abrigo"
+          value={abrigoNome}
+          onChange={(e) => setAbrigoNome(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Endereço do Abrigo"
+          value={abrigoEndereco}
+          onChange={(e) => setAbrigoEndereco(e.target.value)}
+        />
+      </div>
+      <button className="signup-button" onClick={handleCadastro}>
+        Cadastro
+      </button>
     </div>
-    </>
   );
 }
 
