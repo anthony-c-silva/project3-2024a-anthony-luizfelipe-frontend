00a84aa1f1bc19169babcb68e0a97528c84b8cb9
Anthony <anthony.silva@avmb.com.br>
2024-07-08 17:27:49 -0300

    style - melhorias no login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,13 +3,16 @@ import './Login.css';
 import { useNavigate } from 'react-router-dom';
 import Cuidar from '../assets/cuidar.svg';
 import axios from 'axios';
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
+import LoginImage from '../assets/mobile-login-cuate.png'; // Adicione a imagem que deseja usar
+import Loading from './Loading'; // Importe o componente de Loading
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false); // Estado para controlar a exibição do Loading
 
   const handleLogin = async () => {
     if (!email || !password) {
@@ -22,6 +25,8 @@ function Login() {
       senha: password,
     };
 
+    setLoading(true); // Ativa o Loading antes de fazer a requisição
+
     try {
       const response = await axios.post('https://project3-2024a-anthony-luizfelipe-backend.onrender.com/login', data);
       const { token } = response.data;
@@ -33,33 +38,43 @@ function Login() {
       navigate(`/dashboard-abrigo/${id}`);
     } catch (error) {
       setError('Erro ao fazer login. Verifique suas credenciais.');
+    } finally {
+      setLoading(false); // Desativa o Loading após a requisição
     }
   };
 
   return (
-    <div className="login-container">
-      <div>
-        <img src={Cuidar} className="logo react" alt="React logo" />
+    <div className="login-page">
+      <div className="login-image-container">
+        <img src={LoginImage} alt="Login visual" className="login-image" />
       </div>
-      <h2 className="titulo">Login</h2>
-      {error && <p className="error-message">{error}</p>}
-      <div>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <div className="login-wrapper">
+        <div className="login-container">
+         
+          <img src={Cuidar} className="logo" alt="React logo" />
+        
+          <h2 className="titulo">Login</h2>
+          {error && <p className="error-message">{error}</p>}
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="login-button" onClick={handleLogin}>Entrar</button>
+        </div>
       </div>
-      <div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button className="login-button" onClick={handleLogin}>Login</button>
+      {loading && <Loading />}
     </div>
   );
 }
