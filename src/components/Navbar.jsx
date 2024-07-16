e1cf55cc0b49bea2b65a70d48992cc4d5017f84b
Anthony <anthony.silva@avmb.com.br>
2024-07-16 10:05:49 -0300

    feat - removendo token ao sair

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {jwtDecode} from "jwt-decode";
 import Avatar from 'react-avatar';
 import './Navbar.css';
 import logo from '../assets/cuidar.svg';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const token = localStorage.getItem('token');
   let abrigoId;
   let nomeUser;
@@ -19,6 +20,11 @@ const Navbar = () => {
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
   };
 
   return (
@@ -39,8 +45,8 @@ const Navbar = () => {
             </div>
             {dropdownOpen && (
               <div className="dropdown-menu">
-                <Link to="#" className="dropdown-item ">Perfil</Link>
-                <Link to="/" className="dropdown-item">Sair</Link>
+                <Link to="#" className="dropdown-item">Perfil</Link>
+                <Link to="/" className="dropdown-item" onClick={handleLogout}>Sair</Link>
               </div>
             )}
           </li>
